220aa094b973cace571bd549552d8183d25db9f7
xnhh <xnhuang@zju.edu.cn>
2021-09-30 23:24:00 +0800

    string2bytearray

--- request/requireBlock.go
+++ request/requireBlock.go
@@ -4,23 +4,27 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
-	. "shawnH/blockverifier"
+	. "shawnH/blockverifier/model"
 )
 
 const url = "https://blockchain.info/rawblock/"
 
-func requireBlockInfo(blockhash string) *Block {
+func RequireBlockInfo(blockhash string) *Block {
 	url := fmt.Sprintf("%s%s", url, blockhash)
 	// fmt.Printf(url)
 	res, err := HttpGet(url)
 	if err != nil {
 		log.Fatalf(err.Error())
+		return nil
 	}
 	block := Block{}
 	jsonErr := json.Unmarshal([]byte(res), &block)
 	if jsonErr != nil {
 		log.Fatalf(jsonErr.Error())
+		return nil
 	}
+
+	// log.Printf("received block: %+v \n", block)
 
 	return &block
 }
--- request/requireBlock_test.go
+++ request/requireBlock_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestRequireBlock(t *testing.T) {
 	blockhash := "0000000000000bae09a7a393a8acded75aa67e46cb81f7acaa5ad94f9eacd103"
-	block := requireBlockInfo(blockhash)
+	block := RequireBlockInfo(blockhash)
 	fmt.Println(block.Hash)
 	fmt.Println(block.MerkleRoot)
 }
